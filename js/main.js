5f7a0ddb13c317ddc54fef62c637789d04ce40e1
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-01-30 15:18:21 +0700

    Portof - resume

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
+// Toggle & Responsive Navigation
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const navList = document.querySelector("nav");
+
+  burger.addEventListener("click", () => {
+    navList.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger");
+  });
+};
+
+navSlide();
+
 // clear from before unload
 window.onbeforeunload = () => {
-    for (const form of document.getElementsByTagName("form")) {
-        form.reset();
-    }
-}
+  for (const form of document.getElementsByTagName("form")) {
+    form.reset();
+  }
+};
